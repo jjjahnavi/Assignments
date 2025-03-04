f97707f6ed38611273cc0c2f8a7f0467da73d8d0
jjjahnavi <jahnavisingampalli33@gmail.com>
2025-03-04 15:28:19 +0530

    Update slc,mlc.py

--- slc,mlc.py
+++ slc,mlc.py
@@ -1,6 +1,2 @@
 print("# This is a single-line comment")
-print('''"""
-This is a multi-line comment.
-It spans multiple lines.
-Used for documentation or explanations.
-"""''')
+print('''"""This is a multi-line comment"""''')
